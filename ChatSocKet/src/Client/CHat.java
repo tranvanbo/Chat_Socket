86796ff4adfbbc89436e734459b4fa3cbd07ab6e
tranvanbo <65969167+tranvanbo@users.noreply.github.com>
2021-12-25 21:38:43 +0700

    test

---
+++ ChatSocKet/src/Client/CHat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+/**
+ *
+ * @author botra
+ */
+public class CHat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ChatFrame sc =new ChatFrame();
+        sc.setVisible(true);
+    }
+    
+}
